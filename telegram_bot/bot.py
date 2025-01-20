02f3414bc0f382c677fc06e7f8b729e254012c1c
Bazch <mikael.vidal@gmail.com>
2025-01-20 15:30:47 -0300

    Vesão funcional do codigo

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,17 +1,10 @@
 import asyncio
-import logging
-import os
-from flask import Flask
-from hypercorn.asyncio import serve
-from hypercorn.config import Config
-from core.api import APIFootball
-from telegram import Bot
+import sys
+sys.path.append("..")  # Adiciona o diretório pai ao caminho de importação
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from telegram import Bot
+from core.api import APIFootball  # Certifique-se de que APIFootball esteja configurada corretamente
 from pytz import timezone
-
-logging.basicConfig(level=logging.INFO)
-
-app = Flask(__name__)
 
 class TelegramBot:
     def __init__(self, api_football, telegram_token, chat_id):
@@ -23,20 +16,53 @@ class TelegramBot:
     async def enviar_mensagem(self, mensagem):
         """Envia uma mensagem para o chat do Telegram."""
         try:
-            logging.info(f"Enviando mensagem: {mensagem}")
             await self.bot.send_message(chat_id=self.chat_id, text=mensagem)
-            logging.info("Mensagem enviada com sucesso")
         except Exception as e:
-            logging.error(f"Erro ao enviar mensagem: {e}")
+            print(f"Erro ao enviar mensagem: {e}")
 
-    async def start_message(self):
-        """Envio de mensagem inicial quando o bot começa"""
-        await self.enviar_mensagem("Starting analysis...")
+    async def enviar_jogos_do_dia(self):
+        """Busca e envia a lista de jogos do dia."""
+        print("\n=== Enviando jogos do dia ===\n")
+        jogos = self.api_football.listar_jogos_do_dia()
+        if jogos:
+            mensagem = "Football games today:\n"
+            for jogo in jogos:
+                time_casa = jogo.get("time_casa", "Desconhecido")
+                time_fora = jogo.get("time_fora", "Desconhecido")
+                hora_jogo = jogo.get("hora_jogo", "Data não disponível")
+                mensagem += f"{time_casa} x {time_fora} - Time: {hora_jogo}\n"
+            await self.enviar_mensagem(mensagem)
+        else:
+            print("Não há jogos para hoje.")
+
+    async def monitorar_jogos(self):
+        """Monitora jogos ao vivo no primeiro tempo e envia apostas encontradas."""
+        print("\n=== Monitorando jogos ao vivo no primeiro tempo ===\n")
+        jogos = self.api_football.listar_jogos_HT()
+        if jogos:
+            for jogo in jogos:
+                time_favorito = self.api_football.verificar_criterios(jogo)  # Obter o time favorito
+                if time_favorito:  # Se houver um time favorito que atenda aos critérios
+                    time_casa = jogo["teams"]["home"]["name"]
+                    time_fora = jogo["teams"]["away"]["name"]
+
+                    aposta = (
+                        f"BET FOUND:\n\n"
+                        f"{time_casa} x {time_fora}\n"
+                        f"Site: www.orbitxch.com\n"
+                        f"Bet on team: {time_favorito} +0.5 gols HT\n\nBET NOW"
+                    )
+                    await self.enviar_mensagem(aposta)
+                else:
+                    print("Jogo não satisfez os critérios")
+        else:
+            print("Não há jogos ao vivo no momento.")
+        
 
 async def job_jogos_do_dia(api_football, telegram_bot):
     """Executa os jobs do bot."""
     await telegram_bot.enviar_jogos_do_dia()
-
+    
 async def job_monitorar(api_football, telegram_bot):
     """Executa os jobs do bot."""
     await telegram_bot.monitorar_jogos()
@@ -45,14 +71,10 @@ async def start_scheduler(api_football, telegram_bot):
     """Inicia o agendador para executar jobs."""
     scheduler = AsyncIOScheduler(timezone=timezone('Europe/London'))
     scheduler.add_job(job_jogos_do_dia, "cron", hour=13, minute=44, args=[api_football, telegram_bot])
-    scheduler.add_job(job_monitorar, "interval", seconds=5, args=[api_football, telegram_bot])
+    scheduler.add_job(job_monitorar,"interval",seconds=5,args=[api_football,telegram_bot])
     scheduler.start()
     await asyncio.Event().wait()
 
-@app.route("/")
-def health_check():
-    """Endpoint para checar se o serviço está ativo."""
-    return {"status": "running"}, 200
 
 def main():
     # Definir sua chave da API e token do Telegram
@@ -66,18 +88,8 @@ def main():
     # Inicializa a classe TelegramBot
     telegram_bot = TelegramBot(api_football, telegram_token, chat_id)
 
-    # Inicia o scheduler em segundo plano
-    asyncio.get_event_loop().create_task(start_scheduler(api_football, telegram_bot))
-
-    # Envia mensagem inicial ao começar
-    asyncio.get_event_loop().create_task(telegram_bot.start_message())
-
-    # Configuração do Hypercorn
-    config = Config()
-    config.bind = ["0.0.0.0:" + os.getenv("PORT", "8080")]
-    
-    # Inicia o servidor com Hypercorn de forma assíncrona
-    asyncio.run(serve(app, config))
+    # Rodar o scheduler usando asyncio.run()
+    asyncio.run(start_scheduler(api_football, telegram_bot))  # Utiliza asyncio.run() para executar o loop
 
 if __name__ == "__main__":
     main()
